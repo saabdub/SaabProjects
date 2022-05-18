9c44baba89b94c56749f3b572b31c5ee3e47a859
Szabolcs Kerepesi <szabolcs.kerepesi@improvit.com>
2022-05-18 19:23:55 +0100

    Binary Tree Project Start

---
+++ main.py
@@ -0,0 +1,25 @@
+
+"""
+Simple binary search tree implementation.
+
+"""
+
+class treeNode:
+    def __init__(self, data):
+        self.root = data
+        self.left = None
+        self.right = None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
