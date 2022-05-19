f96451f05d626aa967f43f0bc7959589c4840bda
Szabolcs Kerepesi <k.szabolcs88@gmail.com>
2022-05-19 08:35:16 +0100

    Commentst added to the treeNode

--- BinaryTree.py
+++ BinaryTree.py
@@ -5,22 +5,45 @@ Simple binary search tree implementation.
 """
 
 class treeNode:
-    def __init__(self, data):
-        self.root = data
+    """
+    Inserts a new node into the tree.
+    """
+    def __init__(self):
+        self.root = None
         self.left = None
         self.right = None
 
-    def is_empty(self) -> bool:
-        return self.root == self.left == self.right == None
 
-    def is_leaf(self) -> bool:
-        return self.left == self.right == None
+def is_empty(btree: treeNode) -> bool:
+    """
+    Returns True if the tree is empty.
+    :param btree: which is a treeNode
+    :return: bool
+    """
+    return btree.root == btree.left == btree.right == None
 
-    def size(self)-> int:
-        if self.is_empty():
-            return 0
-        else:
-            return 1 + self.left.size() + self.right.size()
+
+def is_leaf(btree: treeNode) -> bool:
+    """
+    Returns True if the tree is a leaf.
+    :param btree: which is a treeNode
+    :return: bool
+    """
+    return btree.left == btree.right == None
+
+
+def size(btree: treeNode) -> int:
+    """
+    Returns the size of the tree.
+    :param btree: which is a treeNode
+    :return: int
+    """
+    if is_empty(btree):
+        return 0
+    elif is_leaf(btree):
+        return 1
+    else:
+        return 1 + size(btree.left) + size(btree.right)
 
 
 
