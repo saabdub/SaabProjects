b4b82c5f0c26f53d708090107d909cc689c053a7
Szabolcs Kerepesi <k.szabolcs88@gmail.com>
2022-05-19 21:10:21 +0100

    Comments are added , Functions test is added to test the functionality of the treeNode

--- BinaryTreeTest.py
+++ BinaryTreeTest.py
@@ -1,7 +1,13 @@
 import BinaryTree as b
+import random
 
 
 def classtest(item):
+    """
+    Test the BinaryTree class
+    :param item:
+    :return: None
+    """
     tree = b.treeNode()
     assert b.is_empty(tree) == True
     assert b.is_leaf(tree) == True
@@ -11,13 +17,31 @@ def classtest(item):
     assert b.is_empty(tree2) == False
     assert b.is_leaf(tree2) == True
     assert b.size(tree2) == 1
+    assert b.is_exists(tree2, "a") == True
+    assert b.is_exists(tree2, "b") == False
 
 
 def functiontest(*args):
-    pass
+    tree = b.treeNode()
+    randomlist = [random.randrange(1, 100, 1) for i in range(10)]
+    print(randomlist)
+    for item in randomlist:
+        if b.is_exists(tree, item):
+            print("{} is already in the tree".format(item))
+            count = b.lookup(tree, item)
+            b.insertIntoTree(tree, item, count + 1)
+            assert b.lookup(tree, item) == count + 1
+        else:
+            b.insertIntoTree(tree, item, 1)
+
+
+    b.printInOrder(tree)
+
+
 
 
 def mainTest(item):
+
     classtest(item)
     functiontest(item)
 
