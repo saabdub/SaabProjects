7f0d18376a2e326a77161d7f3e89694f64d07459
szabolcs.kerepesi@improvit.com <szabolcskerepesi@Szabolcss-MacBook-Pro.local>
2023-01-28 21:54:40 +0000

    add the routes for the new pages and add the redirections

--- webapp/WebApp.py
+++ webapp/WebApp.py
@@ -1,7 +1,8 @@
 """ webapp created with flask can receive requests from the browser and send responses back to the browser """
 
 from flask import Flask, request, jsonify, render_template, abort
-from flask import session,redirect
+from flask import session,redirect,url_for
+import hashlib
 from sqlite3 import dbapi2 as sqlite3
 
 
@@ -18,8 +19,9 @@ def connect_db():
 def init_clienttable():
     """Initializes the database."""
     db = connect_db()
-    db.execute('create table if not exists clienttable (id integer primary key, email text, password text, name text, address text, phone text)')
+    db.execute('create table if not exists clienttable (id text primary key, email text, password text, name text, address text, phone text)')
     db.commit()
+    db.close()
 
 init_clienttable()
 
@@ -27,6 +29,7 @@ def get_clienttable(email, password):
     db = connect_db()
     cur = db.execute('select * from clienttable where email = ? and password = ?', [email, password])
     entries = cur.fetchall()
+    entries = [dict(id=row[0], email=row[1], password=row[2], name=row[3], address=row[4], phone=row[5]) for row in entries][0]
     return entries
 
 def insert_clienttable(id, email, password, name, phone):
@@ -34,6 +37,7 @@ def insert_clienttable(id, email, password, name, phone):
     db.execute('insert into clienttable (id, email, password, name, phone) values (?, ?, ?, ?, ?)',
                  [id, email, password, name, phone])
     db.commit()
+    db.close()
     return "Successfull Register"
 
 
@@ -58,28 +62,31 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    if "Client" in session:
+        return redirect(url_for("user"))
+
+    elif request.method == 'POST' :
         data = request.form.to_dict()
-        data["password"] = data["password"].__hash__()
+        data["password"] = hashlib.sha256(data["password"].encode()).hexdigest()
         all_data = get_clienttable(data["email"], data["password"])
         if len(all_data) > 0:
-            return redirect("/Admin.html")
+            session["Client"] = all_data["id"]
+            return redirect(url_for("user"))
         else:
             return redirect("/login")
 
     elif request.method == 'GET':
         return render_template("login.html")
     
-
 # register on the webpages
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         data = request.form.to_dict()
-        data["password"] = data["password"].__hash__()
-        data["id"] = len(clients).__hash__()
+        data["password"] = hashlib.sha256(data["password"].encode()).hexdigest()
+        data["id"] = hashlib.sha256(data["email"].encode()).hexdigest()
         insert_clienttable(data["id"], data["email"], data["password"], data["username"], data["phone"])
-        
+
         return redirect("/login")
     elif request.method == 'GET':
         return render_template("register.html")
@@ -87,10 +94,19 @@ def register():
         "redirect to login"
         return redirect("/login", code=400)
     
+@app.route('/user', methods=['GET'])
+def user():
+    if "Client" in session:
+        return render_template("admin.html", data=session["Client"])
+    else:
+        return redirect("/login")
 
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop("Client", None)
+    return redirect("/login")
 
 @app.route('/post', methods=['POST'])
-
 def posting():
     print("request.headers", request.headers)
 
@@ -108,7 +124,24 @@ def posting():
        
     else :
         return jsonify({'Authorization': 'Faild!'})
+
+@app.route('/query', methods=['POST'])
+def query():
+    query = request.json['query']
+    # Connect to database and execute query
+    conn = sqlite3.connect("webapp/database.db")
+    cursor = conn.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    #get the column names
+    colnames = [desc[0] for desc in cursor.description] 
+    # add the column names to the result 1st item   
+    result.insert(0, colnames)
     
+    # Return result as JSON
+    return jsonify(result)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
