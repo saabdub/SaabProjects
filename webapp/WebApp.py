364d31ea8090ac8caea236d8f54611811c84e8ec
szabolcs.kerepesi@improvit.com <szabolcskerepesi@Szabolcss-MacBook-Pro.local>
2023-01-28 21:47:09 +0000

    Removing FavouriteRecursion.py

---
+++ webapp/WebApp.py
@@ -0,0 +1,120 @@
+""" webapp created with flask can receive requests from the browser and send responses back to the browser """
+
+from flask import Flask, request, jsonify, render_template, abort
+from flask import session,redirect
+from sqlite3 import dbapi2 as sqlite3
+
+
+
+# create the database if not exist
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect('webapp/database.db')
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+# create the clienttable if not exist
+def init_clienttable():
+    """Initializes the database."""
+    db = connect_db()
+    db.execute('create table if not exists clienttable (id integer primary key, email text, password text, name text, address text, phone text)')
+    db.commit()
+
+init_clienttable()
+
+def get_clienttable(email, password):
+    db = connect_db()
+    cur = db.execute('select * from clienttable where email = ? and password = ?', [email, password])
+    entries = cur.fetchall()
+    return entries
+
+def insert_clienttable(id, email, password, name, phone):
+    db = connect_db()
+    db.execute('insert into clienttable (id, email, password, name, phone) values (?, ?, ?, ?, ?)',
+                 [id, email, password, name, phone])
+    db.commit()
+    return "Successfull Register"
+
+
+clients = []
+
+
+
+
+app = Flask(__name__)
+app.secret_key = "mysecretkey"
+datainfo = []
+session = {}
+
+
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        data["password"] = data["password"].__hash__()
+        all_data = get_clienttable(data["email"], data["password"])
+        if len(all_data) > 0:
+            return redirect("/Admin.html")
+        else:
+            return redirect("/login")
+
+    elif request.method == 'GET':
+        return render_template("login.html")
+    
+
+# register on the webpages
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        data["password"] = data["password"].__hash__()
+        data["id"] = len(clients).__hash__()
+        insert_clienttable(data["id"], data["email"], data["password"], data["username"], data["phone"])
+        
+        return redirect("/login")
+    elif request.method == 'GET':
+        return render_template("register.html")
+    else:
+        "redirect to login"
+        return redirect("/login", code=400)
+    
+
+
+@app.route('/post', methods=['POST'])
+
+def posting():
+    print("request.headers", request.headers)
+
+    def Auth():
+        if app.secret_key == request.headers["Authorization"]:
+            return True
+        else:
+            return False
+
+    if Auth():
+        # get auth session key 
+        # if session key is not in session then create a new session key
+        session["Client"] = 123
+        return jsonify({"status": "OK"})
+       
+    else :
+        return jsonify({'Authorization': 'Faild!'})
+    
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
+
+
+
+
+
